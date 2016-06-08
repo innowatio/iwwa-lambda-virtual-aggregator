3b34410582bca40d465cb6677670f3461898b388
Emanuele Busi <emanuele.busi@mondora.com>
2016-06-08 23:22:51 +0200

    Added AGGREGATES_COLLECTION_NAME as environment parameter

--- src/config.js
+++ src/config.js
@@ -9,6 +9,6 @@ export const KINESIS_STREAM_NAME = process.env.KINESIS_STREAM_NAME || "test";
 export const ALLOWED_SOURCES = ["reading"];
 export const DEFAULT_SAMPLE_DELTA_IN_MS = moment.duration(5, "minutes").asMilliseconds();
 export const FORMULAS_COLLECTION = "virtual-sensors-formulas";
-export const AGGREGATES_COLLECTION_NAME = "readings-daily-aggregates";
+export const AGGREGATES_COLLECTION_NAME = process.env.AGGREGATES_COLLECTION_NAME || "readings-daily-aggregates";
 export const PRODUCER = "iwwa-lambda-virtual-aggregator";
 export const SENSOR_INSERT = "element inserted in collection readings";
